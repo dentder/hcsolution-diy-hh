b30032380e18ebbd095afaadc2945aba6de78234
hoanghai19487@gmail.com <hoanghai19487@gmail.com@0c52e64d-1388-d15d-a40f-05f240a26b2c>
2014-05-07 13:07:07 +0000

    update read recipe version

--- V3_Galileo/Code/NodeJS/test_HH2.py
+++ V3_Galileo/Code/NodeJS/test_HH2.py
@@ -4,7 +4,9 @@ from PIL import Image
 print "Reading file..."
 # Read a Data Matrix barcode
 dm_read = DataMatrix()
-img = Image.open("Cropped1.jpg")
+img = Image.open("test/test08.jpg")
+(w,h) = img.size
+print w,h
 
 print dm_read.decode(img.size[0], img.size[1], buffer(img.tostring()))
 print dm_read.count()
